2d7b202a9c90508b2e8b944c8f52e34409a71eb0
vkehfdl1 <vkehfdl1@gmail.com>
2022-05-23 17:31:16 +0900

    new plan activity

---
+++ app/src/main/java/com/example/perfectscheduler/PlanActivity.kt
@@ -0,0 +1,11 @@
+package com.example.perfectscheduler
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plan)
+    }
+}
